63e03a448cf378c5f5d373e13aef82a162a8d09a
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-11-18 17:10:53 -0300

    start implementing google maps

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@
 import { Company } from './Company';
 import { User } from './User';
 
+console.log('something');
+
 const newUser = new User();
 const newCompanny = new Company();
 
-console.log(newUser);
-console.log(newCompanny);
+const mapDiv = document.getElementById('map') as HTMLElement;
+
+console.log(mapDiv);
+
+new google.maps.Map(mapDiv, {
+  zoom: 0,
+  center: { lat: 0, lng: 0 },
+});
