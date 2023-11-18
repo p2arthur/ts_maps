5d2f530c6eaf1dfc7797594ab08a7594e782f44a
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-11-18 13:21:16 -0300

    implement catchPhrase to the company object

--- src/Company.ts
+++ src/Company.ts
@@ -2,10 +2,12 @@ import { faker } from '@faker-js/faker';
 
 export class Company {
   name: string;
+  catchPhrase: string;
   location: { lat: number; lng: number };
 
   constructor() {
     this.name = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: faker.location.latitude(),
       lng: faker.location.longitude(),
